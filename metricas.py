79c63e8cdf2ff2f7f1ae8668377dedbc23c2b581
igorruys <igorruys@gmail.com>
2025-03-01 20:30:41 -0300

    pequenas correções

--- metricas.py
+++ metricas.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import fbeta_score
+from numpy import sqrt
 
 def custom_fbeta(y_true, y_pred):
-    return fbeta_score(y_true, y_pred, beta=1.22)
+    return fbeta_score(y_true, y_pred, beta=2)
 
